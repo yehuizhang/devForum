31ed3a3291a84b4608624bfceffadb488de08d2c
Yehui <yehuizhang@outlook.com>
2019-11-17 00:10:57 -0800

    Adding endpoints

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+connectDB();
 
 app.get('/', (req, res) => res.send('API is Running'));
+
+// Define Routes
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/users', require('./routes/api/users'));
+
 
 const PORT = process.env.PORT || 5000;
 
