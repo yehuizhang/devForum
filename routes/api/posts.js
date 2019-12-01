374cccd25d54eb624ece1285ed5e5bdb075be212
Yehui <yehuizhang@outlook.com>
2019-12-01 02:42:03 -0800

    Feature: Like/unlike a post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -6,37 +6,42 @@ const Post = require('../../models/Post');
 const User = require('../../models/User');
 const { serverError } = require('./helper/errorHandling');
 
-
 const router = express.Router();
 
 // @route   POST api/posts
 // @desc    Create a post
 // @access  private
-router.post('/', [
-  auth,
-  check('text', 'Text is required').not().isEmpty(),
-], async (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
+router.post(
+  '/',
+  [
+    auth,
+    check('text', 'Text is required')
+      .not()
+      .isEmpty(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      });
+
+      const post = await newPost.save();
+      return res.json(post);
+    } catch (error) {
+      serverError(error, res);
+    }
   }
-
-  try {
-    const user = await User.findById(req.user.id).select('-password');
-
-    const newPost = new Post({
-      text: req.body.text,
-      name: user.name,
-      avatar: user.avatar,
-      user: req.user.id,
-    });
-
-    const post = await newPost.save();
-    return res.json(post);
-  } catch (error) {
-    serverError(error, res);
-  }
-});
+);
 
 // @route   GET api/posts
 // @desc    Get all posts
@@ -78,7 +83,9 @@ router.delete('/:id', auth, async (req, res) => {
       return res.status(404).json({ message: 'Post not found!' });
     }
     if (post.user.toString() !== req.user.id) {
-      return res.status(401).json({ message: 'User is not authorized to delete this post' });
+      return res
+        .status(401)
+        .json({ message: 'User is not authorized to delete this post' });
     }
     await post.remove();
 
@@ -87,6 +94,58 @@ router.delete('/:id', auth, async (req, res) => {
     if (error.kind === 'ObjectId') {
       return res.status(404).json({ message: 'Post not found!' });
     }
+    serverError(error, res);
+  }
+});
+
+// @route   PUT api/posts/like/:id
+// @desc    Like a post
+// @access  Private
+router.put('/like/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ message: 'Post not found!' });
+    }
+
+    if (
+      post.likes.filter(like => like.user.toString() === req.user.id).length > 0
+    ) {
+      return res.status(400).json({ message: 'Post already liked' });
+    }
+    post.likes.push({
+      user: req.user.id,
+    });
+    await post.save();
+    return res.json(post.likes);
+  } catch (error) {
+    serverError(error, res);
+  }
+});
+
+// @route   PUT api/posts/unlike/:id
+// @desc    unlike a post
+// @access  Private
+router.put('/unlike/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ message: 'Post not found!' });
+    }
+
+    if (
+      post.likes.filter(like => like.user.toString() === req.user.id).length ===
+      0
+    ) {
+      return res.status(400).json({ message: 'Post has not yet been liked' });
+    }
+
+    post.likes = post.likes.filter(
+      like => like.user.toString() !== req.user.id
+    );
+    await post.save();
+    return res.json(post.likes);
+  } catch (error) {
     serverError(error, res);
   }
 });
