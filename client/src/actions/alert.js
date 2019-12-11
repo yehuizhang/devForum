0733a87be6f2b5ce4bde6e7cfda753bcab6ce3c2
Yehui <yehuizhang@outlook.com>
2019-12-11 15:13:38 -0800

    Alert: set timeout

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -1,7 +1,7 @@
 import uuid from 'uuid/v4';
 import { SET_ALERT, REMOVE_ALERT } from './types';
 
-export const setAlert = (msg, alertType) => dispatch => {
+export const setAlert = (msg, alertType, timeout = 3000) => dispatch => {
   const id = uuid();
   dispatch({
     type: SET_ALERT,
@@ -11,4 +11,7 @@ export const setAlert = (msg, alertType) => dispatch => {
       id,
     },
   });
+
+  // remove the alert 3 seconds after it's dispatched
+  setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), timeout);
 };
